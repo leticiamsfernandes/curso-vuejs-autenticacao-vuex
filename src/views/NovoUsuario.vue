c6ad57eeb805cd8787ffcecdfa52157d8d8e2dc0
leticiamsfernandes <lemayara16@gmail.com>
2021-10-07 09:59:11 -0300

    Registrando Usuários

---
+++ src/views/NovoUsuario.vue
@@ -0,0 +1,44 @@
+<template>
+   <div class="container">
+        <h1>Novo Usuário</h1>
+        <form @submit.prevent="enviarFormulario">
+            <div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" v-model="usuario.nome" />
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control" v-model="usuario.email" />
+            </div>
+            <div class="form-group">
+                <label for="senha">Senha</label>
+                <input type="password" class="form-control" v-model="usuario.senha" />
+            </div>
+            <button class="btn btn-primary" type="submit">Salvar</button>
+        </form>
+    </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            usuario: {
+                nome: "",
+                email: "",
+                senha: "",
+            },
+        };
+    },
+    methods: {
+        enviarFormulario() {
+            axios.post("http://localhost:8000/auth/register", this.usuario)
+                .then(response => console.log(response))
+                .catch(err => console.log(err));
+        },
+    },
+};
+</script>
+<style scoped>
+</style>
